b229207a58b3c03a784f0cd84f6f64c7e216ab9e
Joseph David <jodavid444dave@gmail.com>
2022-12-16 04:58:31 -0800

    Add event and logic to project

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,48 +1,57 @@
 import React from 'react';
 
+import calculate from '../logic/calculate';
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  buttonClick(element) {
+    this.setState((state) => calculate(state, element.target.textContent));
   }
 
   render() {
+    const { next } = this.state;
+    const { total } = this.state;
     return (
       <div className="button-container">
-        <div className="calc-section">0</div>
+        <div className="calc-section">{next || total || 0}</div>
         <div className="grid-container">
           <div className="button-container-row">
-            <button type="button" className="gray-button">AC</button>
-            <button type="button" className="gray-button">+/-</button>
-            <button type="button" className="gray-button">%</button>
-            <button type="button" className="orange-button">÷</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">AC</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">+/-</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">%</button>
+            <button type="button" onClick={this.buttonClick} className="orange-button">÷</button>
           </div>
 
           <div className="button-container-row">
-            <button type="button" className="gray-button">7</button>
-            <button type="button" className="gray-button">8</button>
-            <button type="button" className="gray-button">9</button>
-            <button type="button" className="orange-button">x</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">7</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">8</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">9</button>
+            <button type="button" onClick={this.buttonClick} className="orange-button">x</button>
           </div>
 
           <div className="button-container-row">
-            <button type="button" className="gray-button">4</button>
-            <button type="button" className="gray-button">5</button>
-            <button type="button" className="gray-button">6</button>
-            <button type="button" className="orange-button">-</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">4</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">5</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">6</button>
+            <button type="button" onClick={this.buttonClick} className="orange-button">-</button>
           </div>
 
           <div className="button-container-row">
-            <button type="button" className="gray-button">1</button>
-            <button type="button" className="gray-button">2</button>
-            <button type="button" className="gray-button">3</button>
-            <button type="button" className="orange-button">+</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">1</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">2</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">3</button>
+            <button type="button" onClick={this.buttonClick} className="orange-button">+</button>
           </div>
 
           <div className="third-row-bottom">
-            <button type="button" className="gray-button">0</button>
-            <button type="button" className="gray-button">.</button>
-            <button type="button" className="orange-button">=</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">0</button>
+            <button type="button" onClick={this.buttonClick} className="gray-button">.</button>
+            <button type="button" onClick={this.buttonClick} className="orange-button">=</button>
           </div>
         </div>
 
